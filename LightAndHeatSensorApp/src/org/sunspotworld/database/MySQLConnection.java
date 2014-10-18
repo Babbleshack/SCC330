f3a8782f79e7cf2e17b3bbb3927f0eecb67fdfca
Babblebase <Babblebase@192.168.0.8>
2014-10-18 11:57:31 +0100

    connection class done

--- LightAndHeatSensorApp/src/org/sunspotworld/database/MySQLConnection.java
+++ LightAndHeatSensorApp/src/org/sunspotworld/database/MySQLConnection.java
@@ -5,10 +5,51 @@
  */
 package org.sunspotworld.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Babblebase
  */
-public class MySQLConnection {
+public class MySQLConnection 
+{
+    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost/SPOT";
+    private static final String USERNAME = "babbleshack";
+    private static final String PASSWORD = "password";
+    private Connection connection = null;
     
+    
+    public MySQLConnection()
+    {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (Exception e) {
+            System.err.println("MySQL driver not found" + e);
+        }
+    }
+    public void connect()
+    {
+        try { 
+            connection  =
+                DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+        } catch (SQLException ex) {
+            System.err.println("Failed to connect to DB" + ex);
+        }
+    }
+    
+    public void disconnect()
+    {
+        try
+        {
+            connection.close();
+        } catch (Exception e) {
+            System.err.println("connection failed to close! " + e);
+        }
+    }  
 }
