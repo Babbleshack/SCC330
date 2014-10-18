70f90408bb96b35d27fb51dd1eee7231a3a72da6
Babblebase <Babblebase@192.168.0.8>
2014-10-18 15:34:57 +0100

    added interfaces for light data

---
+++ LightAndHeatSensorApp/src/org/sunspotworld/DataTypes/IThermoData.java
@@ -0,0 +1,15 @@
+/*
+ * encapsulate Thermonitor data extends ISpotData
+ * Dominic Lindsay
+ */
+package org.sunspotworld.DataTypes;
+
+/**
+ *
+ * @author Babblebase
+ */
+public interface IThermoData 
+{
+    void setThermoData(double data);
+    double getThermoData();
+}
