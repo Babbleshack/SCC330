b0b28d8318cf2cc3a71545a180e9beb58f950f5d
Babbleshack <dcrl94@gmail.com>
2014-10-22 14:09:16 +0100

    fixed light data interface

--- LightAndHeatSensorApp/src/org/sunspotworld/DataTypes/ILightData.java
+++ LightAndHeatSensorApp/src/org/sunspotworld/DataTypes/ILightData.java
@@ -11,14 +11,10 @@ import java.sql.Timestamp;
 public interface ILightData extends ISunSpotData
 {
     String getSpotAddress();
+    Timestamp getTime();
+    double getLightData();
     void setSpotAddress(String spotAddress);
     void setLightData(double data);
-    double getLightData();
-	String getSpotAddress();
-    Timestamp getTime();
     void setTime(Timestamp time);
-    int getZoneId();
-	void setSpotAddress(String setSpotAddress);
-    void setLightData(double data);
-    void setZoneId(int zoneId);
+	
 }
