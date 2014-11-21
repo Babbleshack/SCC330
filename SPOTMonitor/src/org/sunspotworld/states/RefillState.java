d6cdb72f1d16175bae5744a5adecc99d52095fed
babbleshack <dcrl94@gmail.com>
2014-11-21 17:06:49 +0000

    REFILL STATE FUCK ABOUT

--- SPOTMonitor/src/org/sunspotworld/states/RefillState.java
+++ SPOTMonitor/src/org/sunspotworld/states/RefillState.java
@@ -3,10 +3,7 @@
  * @author Dominic Lindsay
  */
 package org.sunspotworld.states;
-
 import org.sunspotworld.spotMonitors.WaterMonitor;
-
-
 public class RefillState implements SmartCupState
 {
     public double pour(WaterMonitor context, double cupAngle, double fillLevelPercentage) {
