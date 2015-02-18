c5bb64c7475e05bb6d3575ff9ec3900a617a7baf
babbleshack <dcrl94@gmail.com>
2015-02-18 14:56:05 +0000

    Made some changes to Threshold Monitor

--- SPOTMonitor/src/org/sunspotworld/monitorStates/AxisThresholdState.java
+++ SPOTMonitor/src/org/sunspotworld/monitorStates/AxisThresholdState.java
@@ -8,8 +8,8 @@ public class AxisThresholdState implements IThresholdMonitorState {
     private static final double LOWER_THRESH = 0.8;
     private static final double UPPER_THRESH = 1.2;
     public void checkThresholdCondition(ThresholdMonitor context) {
-        if(context.getSensorReading().getDataAsDouble() < LOWER_THRESH ||
-                context.getSensorReading().getDataAsDouble() > UPPER_THRESH) 
+        if(context.getSensorReading().getDataAsDouble() > LOWER_THRESH && 
+                context.getSensorReading().getDataAsDouble() < UPPER_THRESH) 
             return;
         else
             context.notifyObservers(context.getSensorReading());
