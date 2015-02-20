8fe5881f8add1fd408deac7fb3b29737a87d1428
babbleshack <dcrl94@gmail.com>
2015-02-20 01:23:45 +0000

    changed zone_spot to ZoneSpot in query

--- SPOTMonitor/src/org/sunspotworld/monitorStates/LightThresholdState.java
+++ SPOTMonitor/src/org/sunspotworld/monitorStates/LightThresholdState.java
@@ -3,7 +3,6 @@ package org.sunspotworld.monitorStates;
  * PhotoMonitor implementation of checkSensorThreshold()
  * @author Dominic Lindsay
  */
-import org.sunspotworld.spotMonitors.IThresholdMonitor;
 import org.sunspotworld.spotMonitors.ThresholdMonitor;
 public class LightThresholdState implements IThresholdMonitorState {
     public void checkThresholdCondition(ThresholdMonitor context) {
